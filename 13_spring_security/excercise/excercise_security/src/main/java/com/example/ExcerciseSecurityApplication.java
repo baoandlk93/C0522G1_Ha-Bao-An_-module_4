731a73fb1707291c4d228e51a3d498cb1ae1b17f
Hà Bảo Ân <you@example.combaoandlk93@gmail.com>
2022-09-30 19:22:17 +0700

    30/09/2022

---
+++ 13_spring_security/excercise/excercise_security/src/main/java/com/example/ExcerciseSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcerciseSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcerciseSecurityApplication.class, args);
+    }
+
+}
--- 13_spring_security/excercise/excercise_security/src/main/java/com/example/config/AppConfig.java
+++ 13_spring_security/excercise/excercise_security/src/main/java/com/example/config/AppConfig.java
@@ -1,2 +1,14 @@
-package com.example.config;public class AppConfig {
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
 }
